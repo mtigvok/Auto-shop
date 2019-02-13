3737665f4ad31662e025494588923e8a0dc13296
mtigvok <mrigvok@gmail.com>
2019-02-14 00:05:14 +0200

    create contoller/model car

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,15 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20190213015216) do
+ActiveRecord::Schema.define(version: 20190213220352) do
+
+  create_table "cars", force: :cascade do |t|
+    t.string "name"
+    t.string "desc"
+    t.integer "price"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+  end
 
   create_table "users", force: :cascade do |t|
     t.string "email", default: "", null: false
